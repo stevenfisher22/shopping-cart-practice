24e31d14e1607c40f70866fb20ed4c497671d9cf
Steven Fisher <steven.fisher22@gmail.com>
2019-02-15 11:46:58 -0600

    add product action

---
+++ src/actions/addProduct.js
@@ -0,0 +1,11 @@
+function addProduct(item) {
+    return {
+        type: 'addProduct',
+        productData: {
+            productName: item.productName, 
+            productPrice: item.productPrice
+        }
+    }
+}
+
+export default addProduct
