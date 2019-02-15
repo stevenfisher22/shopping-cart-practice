35fe604517fd973824dfa4921a07fa611be5dc3c
Steven Fisher <steven.fisher22@gmail.com>
2019-02-15 11:14:20 -0600

    BaseLayout.js

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class BaseLayout extends React.Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default BaseLayout
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import BaseLayout from './components/BaseLayout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <BaseLayout>
+            <div>
+                Hello World
+            </div>
+        </BaseLayout>
+    </BrowserRouter>
+, document.getElementById('root'));
 
