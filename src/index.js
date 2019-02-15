0d88139adfd47485c35da1cbba7b25e73dee04c5
Steven Fisher <steven.fisher22@gmail.com>
2019-02-15 11:07:38 -0600

    setup folders and index.js imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
