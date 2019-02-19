fed46ea75cfa6ed2919314f7cc9a982d1b163eae
Steven Fisher <steven.fisher22@gmail.com>
2019-02-19 14:42:47 -0600

    removed some unused imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import BaseLayout from './components/BaseLayout';
 import cartReducer from './reducers/cartReducer';
-import deleteProduct from './actions/deleteProduct';
 import Cart from './components/Cart';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const store = createStore(
